2530008bb840b6398dfb12f4448c624391b99d0b
GM <giovanna.occhialini@wsu.edu>
2026-03-27 21:00:45 -0700

    Add shuffle function for concentration cards

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -11,3 +11,22 @@ let boardLocked = false;
 let turnCount = 0;
 let matchedPairs = 0;
 
+shuffleCards();
+
+// randomizes the order of the cards on the board
+function shuffleCards() {
+    let cardArray = Array.from(cards);
+
+    for (let i = cardArray.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+
+        let temp = cardArray[i];
+        cardArray[i] = cardArray[randomIndex];
+        cardArray[randomIndex] = temp;
+    }
+
+    cardArray.forEach(function(card) {
+        gameBoard.appendChild(card);
+    });
+}
+
