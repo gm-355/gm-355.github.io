d8a19e04da40a1d2a4a0645ecb162c57fa84d981
GM <giovanna.occhialini@wsu.edu>
2026-04-10 13:51:04 -0700

    update current weather section

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -18,4 +18,14 @@ let url = "https://tordevries.github.io/477/examples/ajax-api-test/current.js";
 
 getData(url).then(function(result) {
     console.log(result);
+    updateCurrentWeather(result);
 });
+
+function updateCurrentWeather(result) {
+    document.querySelector("#location").textContent = result.location.name + ", " + result.location.region;
+    document.querySelector("#tempNow").textContent = result.current.temp_c + "°C";
+    document.querySelector("#conditionTextNow").textContent = result.current.condition.text;
+    document.querySelector("#windNow").textContent = result.current.wind_kph + " km/h";
+    document.querySelector("#humidityNow").textContent = result.current.humidity + "%";
+    document.querySelector("#iconNow").src = "https:" + result.current.condition.icon;
+}
